b7b6615d47d50267232e1ebc301607616393db6d
Anh Phạm <banhbabanhmj123@gmail.com>
2025-03-04 21:33:21 +0700

    done

--- src/routes/client/index.js
+++ src/routes/client/index.js
@@ -7,6 +7,7 @@ import requestExchangeRouter from './requests_exchange.router'
 import notificationRouter from './notification.router'
 import userInterestRouter from './user_interest.router'
 import userInteractionRouter from './user_interaction.router'
+import surveyRouter from './survey.router'
 function routeClient(app) {
     app.use('/auth', authRouter)
 
@@ -25,6 +26,8 @@ function routeClient(app) {
     app.use('/user-interests', userInterestRouter)
 
     app.use('/user-interactions', userInteractionRouter)
+
+    app.use('/surveys', surveyRouter)
 }
 
 export default routeClient
