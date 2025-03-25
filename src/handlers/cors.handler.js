58a74c5e37cc895a0d16f8ec465dcfc62c2b9215
Anh Phạm <banhbabanhmj123@gmail.com>
2025-03-25 22:21:49 +0700

    hotfix cors

--- src/handlers/cors.handler.js
+++ src/handlers/cors.handler.js
@@ -1,12 +1,18 @@
 import cors from 'cors'
 import {APP_URL_CLIENT, OTHER_URLS_CLIENT} from '@/configs'
 
+// export const corsOptions = {
+//     origin: [APP_URL_CLIENT, ...OTHER_URLS_CLIENT],
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH' ,'OPTIONS'], 
+//     allowedHeaders: ['Content-Type', 'Authorization'], 
+// }
+
 export const corsOptions = {
     origin: [APP_URL_CLIENT, ...OTHER_URLS_CLIENT],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH' ,'OPTIONS'], 
-    allowedHeaders: ['Content-Type', 'Authorization'], 
 }
+
 
 const corsHandler = cors(corsOptions)
 
