add413c183814d6e5ba16acbef9b939057bad0e9
Anh Phạm <banhbabanhmj123@gmail.com>
2025-03-26 10:44:46 +0700

    update again cors

--- src/handlers/cors.handler.js
+++ src/handlers/cors.handler.js
@@ -1,17 +1,17 @@
 import cors from 'cors'
 import {APP_URL_CLIENT, OTHER_URLS_CLIENT} from '@/configs'
 
-// export const corsOptions = {
-//     origin: [APP_URL_CLIENT, ...OTHER_URLS_CLIENT],
-//     credentials: true,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH' ,'OPTIONS'], 
-//     allowedHeaders: ['Content-Type', 'Authorization'], 
-// }
-
 export const corsOptions = {
     origin: [APP_URL_CLIENT, ...OTHER_URLS_CLIENT],
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH' ,'OPTIONS'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
 }
+
+// export const corsOptions = {
+//     origin: [APP_URL_CLIENT, ...OTHER_URLS_CLIENT],
+//     credentials: true,
+// }
 
 
 const corsHandler = cors(corsOptions)
